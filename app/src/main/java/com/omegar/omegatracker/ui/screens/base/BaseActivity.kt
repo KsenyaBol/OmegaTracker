99c0c15e106f2cc2a145b2bf85bd6f34664e74c1
shipa <daniil.shipitsyn@omega-r.com>
2021-02-25 18:07:34 +0300

    add splash screen

--- app/src/main/java/com/omegar/omegatracker/ui/screens/base/BaseActivity.kt
+++ app/src/main/java/com/omegar/omegatracker/ui/screens/base/BaseActivity.kt
@@ -1,10 +1,16 @@
 package com.omegar.omegatracker.ui.screens.base
 
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
 import androidx.annotation.ContentView
 import androidx.annotation.LayoutRes
+import com.omega_r.base.annotations.OmegaWindowBackground
 import com.omega_r.base.components.OmegaActivity
 import com.omega_r.base.mvp.presenters.OmegaPresenter
+import com.omega_r.libs.extensions.context.getColorByAttribute
+import com.omegar.omegatracker.R
 
+@OmegaWindowBackground(colorAttrRes = R.attr.activityBackground)
 abstract class BaseActivity : OmegaActivity {
 
     constructor() : super()
@@ -13,4 +19,9 @@ abstract class BaseActivity : OmegaActivity {
     constructor(@LayoutRes contentLayoutId: Int) : super(contentLayoutId)
 
     abstract override val presenter: OmegaPresenter<out BaseView>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.setBackgroundDrawable(ColorDrawable(getColorByAttribute(R.attr.activityBackground)))
+    }
 }
