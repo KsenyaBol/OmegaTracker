a3382e077989c6299d9f5939f94f21ae7517f8fc
bulygin <ivan.bulygin@omega-r.com>
2021-07-14 14:40:47 +0300

    refactoring

--- app/src/main/java/com/omegar/omegatracker/ui/base/BaseActivity.kt
+++ app/src/main/java/com/omegar/omegatracker/ui/base/BaseActivity.kt
@@ -4,13 +4,11 @@ import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.annotation.ContentView
 import androidx.annotation.LayoutRes
-import com.omega_r.base.annotations.OmegaWindowBackground
 import com.omega_r.base.components.OmegaActivity
 import com.omega_r.base.mvp.presenters.OmegaPresenter
 import com.omega_r.libs.extensions.context.getColorByAttribute
 import com.omegar.omegatracker.R
 
-@OmegaWindowBackground(colorAttrRes = R.attr.activityBackground)
 abstract class BaseActivity : OmegaActivity {
 
     constructor() : super()
